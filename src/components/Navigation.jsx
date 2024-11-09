fc025258e5ac894979708f5fecd5e060b1ebdeda
Atharva Todkary <atharvatodkary@gmail.com>
2024-11-09 12:35:31 +0530

    updated nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
-import {RightImages} from './RightImages';
-import {LeftImages} from './LeftImages';
+import {SideImages} from './SideImages';
+
 import {MainContent} from './MainContent';
 
 export const Navigation = () => {
+    const rightIcons = [{
+        id: 1,
+        src: "../images/book.jpg",
+        position: "end"
+    },{
+        id: 2,
+        src: "../images/item.png",
+        position: "start"
+    },{
+        id: 3,
+        src: "../images/smile.png",
+        position: "end"
+    }]
+    const leftIcons = [{
+        id: 1,
+        src: "../images/smile.png",
+        position: "start"
+    },{
+        id: 2,
+        src: "../images/book.jpg",
+        position: "end"
+    },{
+        id: 3,
+        src: "../images/item.png",
+        position: "start"
+    }]
     return (
         <div className="relative z-10 flex justify-between items-center py-6">
-            <RightImages />
+            <SideImages icons={rightIcons}/>
             <MainContent />
-            <LeftImages />
+            <SideImages icons={leftIcons} />
         </div>
     )
 }
---
+++ src/components/SideImages.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ImageIcon } from '../ui/ImageIcon'
+import '../App.css';
+
+
+export const SideImages = (props) => {
+    return (
+        <div className="w-24 sm:w-28 md:w-40 lg:w-48 space-y-8">
+            {props.icons.map((icon, index) =>(
+                <ImageIcon key={index} position={icon.position} src={icon.src} />
+            ))}
+            
+        </div>
+    )
+}
